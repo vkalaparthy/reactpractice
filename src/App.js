66bb6ecbdb2180223fed48d6329ef3084ef9fd5a
vkalaparthy <kalaparthy.vani@gmail.com>
2020-10-29 18:28:06 -0400

    Made chnages to add more quotes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
+import React from 'react';
 import Quotes from './components/Quotes'
 import './App.css';
+import quotesMT from "./quotesMT.json";
+import quotesB from "./quotesB.json";
+import quotesAL from "./quotesAL.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Quotes />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    author: "Mark Twain",
+    count: 0,
+    source: quotesMT[0].quote
+  };
+
+  sendMark = () => {
+    this.setState({ author: "Mark Twain" });
+    this.setState({ count: 0 });
+    this.setState({ source: quotesMT[this.state.count].quote });
+  };
+
+  sendBuddha = () => {
+    this.setState({ author: "Buddha" });
+    this.setState({ count: 0 });
+    this.setState({ source: quotesB[this.state.count].quote });
+  };
+
+  sendAL = () => {
+    this.setState({ author: "Abraham Lincoln" });
+    this.setState({ count: 0 });
+    this.setState({ source: quotesAL[this.state.count].quote });
+  };
+
+  handleChange = () => {
+    console.log(this.state.count);
+    if (this.state.count === 2) {
+      this.setState({ count: 0 });
+    } else {
+      this.setState({ count: this.state.count + 1 });
+    }
+    if ( this.state.author === "Mark Twain" ) {
+      this.setState({ source: quotesMT[this.state.count].quote });
+    } else if ( this.state.author === "Buddha" ){
+      this.setState({ source: quotesB[this.state.count].quote });
+    } else {
+      this.setState({ source: quotesAL[this.state.count].quote });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <span>
+            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.sendMark}>Mark Twain</button>
+            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.sendBuddha}>Buddha</button>
+            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.sendAL}>Abraham Lincoln</button>
+          </span>
+          <br></br>
+          <Quotes 
+            author={this.state.author} 
+            quote={this.state.source}
+            changeQuote={this.handleChange}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -1,36 +1,38 @@
-import React from "react";
 import "./style.css";
-import quotes from "./quotes.json";
 
-class Quotes extends React.Component {
+function Quotes(props) {
 
-  state = {
-    count: 0
-  };
+  // state = {
+  //   count: 0
+  // };
 
-  handleIncrement = () => {
-    console.log(this.state.count);
-    if (this.state.count === 2) {
-      this.setState({ count: 0 });
-    } else {
-      this.setState({ count: this.state.count + 1 });
-    }
-  };
+  // handleIncrement = () => {
+  //   console.log(this.state.count);
+  //   if (this.state.count === 2) {
+  //     this.setState({ count: 0 });
+  //   } else {
+  //     this.setState({ count: this.state.count + 1 });
+  //   }
+  // };
+  
+  // if (props.author === "Mark Twain") {
+  //   quotes = quotesMT;
+  // }
 
-  render() {
+  // render() {
     return (
       <div className="container">
         <div className="jumbotron">
-          <h1>Quotes R'us!</h1>
-          <p> <strong>{quotes[this.state.count].author}</strong></p>
-          <p> {quotes[this.state.count].quote}</p>
+          <h1>Quotes for us!</h1>
+          <p> <strong>{props.author}</strong></p>
+          <h5> {props.quote}</h5>
           <p>
-            <button className="btn btn-secondary btn-lg" onClick={this.handleIncrement}>Change Quote</button>
+            <button className="btn btn-secondary btn-lg" onClick={() => props.changeQuote()}>Change Quote</button>
           </p>
         </div>
       </div>
     )
-  }
+  // }
 }
 
 export default Quotes;
