cc5c3042734657986304c7984d06e99945beb16a
vkalaparthy <kalaparthy.vani@gmail.com>
2020-10-30 08:49:19 -0400

    made minor changes to logic

--- src/App.js
+++ src/App.js
@@ -10,46 +10,55 @@ class App extends React.Component {
   state = {
     author: "Mark Twain",
     count: 0,
-    source: quotesMT[0].quote
+    quote: quotesMT[0].quote
   };
 
-  sendMark = () => {
+  selectMark = () => {
     this.setState({ author: "Mark Twain" });
-    // this.setState({ count: 0 });
-    this.setState({ source: quotesMT[this.state.count].quote });
+    this.setState({ count: 1 });
+    this.setState({ quote: quotesMT[this.state.count].quote});
   };
 
-  sendBuddha = () => {
+  selectBuddha = () => {
     this.setState({ author: "Buddha" });
-    // this.setState({ count: 0 });
-    this.setState({ source: quotesB[this.state.count].quote });
+    this.setState({ count: 0 });
+    // this.setState({ source: quotesB });
+    this.setState({ quote: quotesB[this.state.count].quote });
   };
 
-  sendAL = () => {
+  selectAL = () => {
     this.setState({ author: "Abraham Lincoln" });
-    // this.setState({ count: 0 });
-    this.setState({ source: quotesAL[this.state.count].quote });
+    this.setState({ count: 0 });
+    // this.setState({ source: quotesAL });
+    this.setState({ quote: quotesAL[this.state.count].quote });
   };
 
-  changeQuote = () => {
-    if ( this.state.author === "Mark Twain" ) {
-      this.setState({ source: quotesMT[this.state.count].quote });
-    } else if ( this.state.author === "Buddha" ){
-      this.setState({ source: quotesB[this.state.count].quote });
-    } else {
-      this.setState({ source: quotesAL[this.state.count].quote });
+  nextQuote = () => {
+
+    const { count, author } = this.state;
+
+    switch(author) {
+      case "Mark Twain":
+        this.setState({ quote: quotesMT[count].quote });
+        break;
+      case "Buddha":
+        this.setState({ quote: quotesB[count].quote });
+        break;
+      default:
+        this.setState({ quote: quotesAL[count].quote });
     }
   }
   handleChange = () => {
     console.log(this.state.count);
     if (this.state.count === 2) {
       this.setState({ count: 0 });
-      this.changeQuote();
+      this.nextQuote();
     } else {
       this.setState({ count: this.state.count + 1 });
-      this.changeQuote();
+      console.log(this.state.count);
+      this.nextQuote();
     }
-    console.log(this.state.count);
+    // console.log(this.state.count);
   };
 
   render() {
@@ -57,14 +66,14 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <span>
-            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.sendMark}>Mark Twain</button>
-            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.sendBuddha}>Buddha</button>
-            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.sendAL}>Abraham Lincoln</button>
+            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.selectMark}>Mark Twain</button>
+            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.selectBuddha}>Buddha</button>
+            <button type="button" class="btn btn-success btn-lg mr-4" onClick={this.selectAL}>Abraham Lincoln</button>
           </span>
           <br></br>
           <Quotes 
             author={this.state.author} 
-            quote={this.state.source}
+            quote={this.state.quote}
             changeQuote={this.handleChange}
           />
         </header>
